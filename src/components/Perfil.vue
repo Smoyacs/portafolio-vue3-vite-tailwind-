41a70a5f726970ac33ba651b35d4e4e4ef7ea7cc
smoya <sebastian.moya.dev@gmail.com>
2024-12-04 10:21:14 -0300

    descarga de cv

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -49,6 +49,17 @@
             <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
           </svg>
           LinkedIn
+        </a> <a href="/CV-Sebastian-Moya.pdf" target="_blank" rel="noopener noreferrer" role="link" download
+          class="inline-flex items-center justify-center gap-2 px-4 py-1 text-gray-800 transition bg-gray-100 border border-gray-300 rounded-full dark:bg-gray-800 dark:border-gray-600 dark:text-white focus-visible:ring-yellow-500/80 text-md hover:bg-gray-900 hover:border-gray-700 hover:text-white dark:hover:bg-gray-100 dark:hover:border-gray-300 dark:hover:text-black group max-w-fit focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
+          <svg class="size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+            <path d="M12 17v-6"></path>
+            <path d="M9.5 14.5l2.5 2.5l2.5 -2.5"></path>
+          </svg>
+          Descargar CV
         </a> </nav>
     </div>
   </section>
